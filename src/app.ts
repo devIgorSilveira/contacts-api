63ca94aa07fe06ddbbfaf321206e99f15a38d309
Igor <silveira.igor@outlook.com>
2023-03-24 10:52:16 -0300

    feat: add cors in app

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import "express-async-errors";
 import express from "express";
 import { contactRouter, sessionRouter, usersRouter } from "./routes";
 import { errorHandler } from "./errors/error";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/users", usersRouter);
 app.use("/login", sessionRouter);
